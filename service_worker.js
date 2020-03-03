436d65b67a79d2570d9575e0629d1aaff75b6454
Turkialmalki <turkialmalki202200@gmail.com>
2020-03-03 14:15:05 +0300

    fixing the responsive layout

---
+++ service_worker.js
@@ -0,0 +1,63 @@
+const currentCacheName = 'restaurant-reviews-app-v2';
+
+// Once install phase complete, add all URLs to the cache
+self.addEventListener('install', function(event) {
+
+  const cacheURLs = [
+    '/',
+    '/index.html',
+    '/restaurant.html',
+    '/css/styles.css',
+    '/css/responsive-index.css',
+    '/css/responsive-rest.css',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    '/data/restaurants.json',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg'
+  ];
+
+  event.waitUntil(
+    caches.open(currentCacheName).then(function(cache) {
+      return cache.addAll(cacheURLs);
+    })
+  );
+});
+
+
+// Respond to fetch requests with a page from the cache. 
+// If there's nothing in the cache, go to the network
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
+
+// Delete old caches upon activation of a new SW if there is a new one specified
+self.addEventListener('activate', function(event) {
+	event.waitUntil(
+		caches.keys().then(function(cacheNames) {
+			return Promise.all(
+				cacheNames.filter(function(cacheName) {
+					return cacheName.startsWith('restaurant-reviews-app') && cacheName != currentCacheName;
+				}).map(function(cacheName) {
+					return caches.delete(cacheName);
+				})
+			);
+		})
+	);
+});
---
+++ sw_registration.js
@@ -0,0 +1,5 @@
+// Register service worker (default scope)
+if (navigator.serviceWorker) {
+	navigator.serviceWorker.register('service_worker.js');
+}
+
